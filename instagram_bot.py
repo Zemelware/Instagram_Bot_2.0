ca7c4465848584df5708a8b66977caf0d88a1465
Ethan Zemelman <ethanzemelman1@gmail.com>
2020-11-15 16:55:52 -0500

    Make bot go to Instagram profile when initialized

--- instagram_bot.py
+++ instagram_bot.py
@@ -1,8 +1,25 @@
 from selenium import webdriver
-from time import sleep
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class InstagramBot:
     def __init__(self, username, password):
-        self.driver = webdriver.Chrome()
+        options = webdriver.ChromeOptions()
+        # options.add_argument("--headless")
+        options.add_argument("--start-maximized")
+        self.driver = webdriver.Chrome(chrome_options=options)
         self.driver.get("https://instagram.com")
+
+        self.wait = WebDriverWait(self.driver, 10)
+
+        self.wait.until(EC.presence_of_element_located((By.NAME, "username"))).send_keys(
+            username, Keys.TAB, password, Keys.ENTER)
+
+        # Wait for page to load
+        self.wait.until(EC.invisibility_of_element_located(
+            (By.XPATH, "//button/div[text()='Log In']")))
+
+        self.driver.get(f"https://instagram.com/{username}")
