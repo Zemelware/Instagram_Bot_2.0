855d8755415b092412a958e160777328be2660ed
Ethan Zemelman <ethanzemelman1@gmail.com>
2020-11-15 18:56:42 -0500

    Handle when user inputs '2' to login to different account

--- main.py
+++ main.py
@@ -3,37 +3,42 @@ from instagram_bot import InstagramBot
 
 print("-------------Instagram Bot-------------")
 print("---------------------------------------")
-print("Please enter your Instagram credentials")
-
-username = input("Username (not email): ")
-password = getpass()
-
-print("\n---------------------------------------")
-print("LOGGING IN TO YOUR ACCOUNT...")
-print("---------------------------------------\n")
-
-bot = InstagramBot(username, password)
 
 while True:
-    print("Select an option (type the number)")
-    print("---------------------------------------")
-    print("1. Check who's not following you back")
-    print("2. Use different Instagram account")
-    print("3. Quit")
-    chosen_option = input(": ").rstrip()
+    print("Please enter your Instagram credentials")
 
-    if chosen_option == "1":
-        pass
-    elif chosen_option == "2":
-        # bot.driver.quit()
-        pass
-    elif chosen_option == "3":
-        bot.driver.quit()
-        quit()
-    else:
-        print("\nInvalid option\n")
-        continue
+    username = input("Username (not email): ")
+    password = getpass()
 
     print("\n---------------------------------------")
-    print("THE TASK WAS COMPLETED SUCCESSFULLY")
+    print("LOGGING IN TO YOUR ACCOUNT...")
     print("---------------------------------------\n")
+
+    bot = InstagramBot(username, password)
+
+    while True:
+        print("Select an option (type the number)")
+        print("---------------------------------------")
+        print("1. Check who's not following you back")
+        print("2. Use different Instagram account")
+        print("3. Quit")
+        chosen_option = input(": ").rstrip()
+
+        if chosen_option == "1":
+            pass
+        elif chosen_option == "2":
+            bot.driver.quit()
+            print("")  # This creates a new line
+            break
+        elif chosen_option == "3":
+            bot.driver.quit()
+            quit()
+        else:
+            print("\nInvalid option\n")
+            continue
+
+        print("\n---------------------------------------")
+        print("THE TASK WAS COMPLETED SUCCESSFULLY")
+        print("---------------------------------------\n")
+
+    continue
